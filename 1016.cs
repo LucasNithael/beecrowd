df25d79c14c66b8c8b04efc9165dd59e81466f54
Lucas Nithael <94084548+LucasNithael@users.noreply.github.com>
2024-01-12 21:48:16 -0300

    Distância

---
+++ 1016.cs
@@ -0,0 +1,14 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+       int l = int.Parse(Console.ReadLine());
+       int x = l*2;
+       
+       Console.WriteLine($"{x} minutos");       
+
+    }
+
+}
